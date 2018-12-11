f919c7c3cc84c80915ee2a05688a37281ffb5ff8
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-12-11 00:59:34 -0800

    Add more projects

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ function stickNavBar() {
 }
 
 function openMenu() {
+  console.log('hello');
   if (navbar.className === "nav-bar" && mainnav.className === "main-nav") {
     navbar.className += " responsive";
     mainnav.className += " responsive";
@@ -20,4 +21,41 @@ function openMenu() {
     navbar.className = "nav-bar";
     mainnav.className = "main-nav";
   }
+}
+
+
+var modal = document.getElementById('myModal');
+var images = document.getElementsByClassName('myImages');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  img.addEventListener("click", openModal);
+  function openModal(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+document.getElementsByClassName("close")[0].addEventListener("click", closeModal);
+function closeModal() {
+  modal.style.display = "none";
+}
+
+//PRINTS
+document.getElementById("prints").addEventListener("click", openNews);
+
+function openNews() {
+  document.querySelector('.print').classList.toggle('hide');
+  // document.querySelector('.post').classList.toggle('hide');
+}
+
+// POST
+document.getElementById("posts").addEventListener("click", openPosts);
+
+function openPosts() {
+  document.querySelector('.post').classList.toggle('hide');
+//   document.querySelector('.print').classList.add('hide');
 }
