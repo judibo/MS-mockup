34af3781a59267dc1d8af9fbd9ff3fa4d0b61ba5
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-12-10 16:29:59 -0800

    Make hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickNavBar()};
 
 var navbar = document.getElementById("navbar");
+var mainnav = document.getElementById("mainnav");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+function stickNavBar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+  }
+}
+
+function openMenu() {
+  if (navbar.className === "nav-bar" && mainnav.className === "main-nav") {
+    navbar.className += " responsive";
+    mainnav.className += " responsive";
+  } else {
+    navbar.className = "nav-bar";
+    mainnav.className = "main-nav";
   }
 }
